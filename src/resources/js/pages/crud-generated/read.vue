142bb660cf2e7cf7fe16ef15181e13fcc558a293
bit-business <101217799+bit-business@users.noreply.github.com>
2023-06-12 18:26:25 +0200

    Update read.vue

--- src/resources/js/pages/crud-generated/read.vue
+++ src/resources/js/pages/crud-generated/read.vue
@@ -32,7 +32,7 @@
               <vs-col
                 v-for="(dataRow, rowIndex) in dataType.dataRows"
                 :key="rowIndex"
-                :vs-lg="dataRow.details.size ? dataRow.details.size : '12'"
+                :vs-lg="dataRow.details.size ? dataRow.details.size : '6'"
               >
                 <template v-if="dataRow.read">
                   <table class="skijasi-table">
